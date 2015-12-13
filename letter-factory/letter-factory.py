bf4a84c756b3b815494e3229a224c79841a2bcbc
Marcus <redmnic@gmail.com>
2015-12-13 12:19:59 +0100

    letter-factory: Fix pattern substitution meta-character bug.
    Meta characters in substitution text are inrepreted by re.sub(). This
    leads to a bug, where illegal LaTeX code is generated. This patch fixes
    that bug.
    
    Additional content:
    This patch introduces log-level configuration based on the command-line
    options.

--- letter-factory/letter-factory.py
+++ letter-factory/letter-factory.py
@@ -6,20 +6,12 @@ import yaml
 import re
 import os
 import shutil
+import sys
 from contextlib import ExitStack
 from argparse import ArgumentParser
 from os.path import abspath, join, basename
 from tempfile import TemporaryDirectory
 from logging import debug
-
-# Change to DEBUG for more debugging output.
-logging.basicConfig(level=logging.ERROR, format="%(levelname)s:%(message)s")
-
-def parseArguments():
-    argparser = ArgumentParser(description="A LaTeX letter ftory")
-    argparser.add_argument("-t", help="template file", action="store", dest="templateFile")
-    argparser.add_argument("substitutionFile", help="The file containing the substitutions", action="store")
-    return argparser.parse_args()
 
 KEY_PATTERNS = "patterns"
 KEY_PATTERN = "Exp"
@@ -49,7 +41,10 @@ class SubFile:
 
     def handle(self, line):
         for pattern, sub in self._patterns:
-            return pattern.sub(sub, line)
+            intervals = pattern.finditer(line)
+            for match in intervals:
+                line = line[:match.start()] + sub + line[match.end():]
+            return line
 
     def name(self) -> str:
         return self._name
@@ -69,22 +64,54 @@ def createFiles(outputFileList, tempDir, templateFile):
             open(join(tempDir, x.name()), "w")) for x in outputFileList}
         with open(templateFile) as inputFile:
             for line in inputFile:
+                debug("<InputFile>: %s", line)
                 for outputObject in outputFileList:
                     newLine = outputObject.handle(line)
+                    debug("<%s>: %s", outputObject.name(), newLine)
                     descripors[outputObject.name()].write(newLine)
+
+def parseArguments():
+    argparser = ArgumentParser(description="A LaTeX letter ftory")
+    argparser.add_argument("-t", help="template file", action="store", dest="templateFile")
+    argparser.add_argument("substitutionFile", help="The file containing the substitutions", action="store")
+    argparser.add_argument("-v", action="count", dest="verbosity", help="be more verbose", default=0)
+    argparser.add_argument("--tex-output", dest="texOutput", help="Display output from LaTeX compilation", action="store_true", default=False)
+    return argparser.parse_args()
+
+def setLoggingLevel(arguments):
+    logging.basicConfig(level=logging.ERROR, format="%(levelname)s:%(message)s")
+    logger = logging.getLogger()
+    if arguments.verbosity > 3:
+        arguments.verbosity = 3
+    newLogLevel = int(logging.ERROR - (arguments.verbosity * (logging.ERROR - logging.DEBUG)/3))
+    assert type(newLogLevel) is int
+    logger.setLevel(newLogLevel)
+
 
 def main():
     arguments = parseArguments()
+    setLoggingLevel(arguments)
     substitutionFile = abspath(arguments.substitutionFile)
     outputFileList = readSubstitutions(substitutionFile)
     with TemporaryDirectory() as tempDir:
         createFiles(outputFileList, tempDir, abspath(arguments.templateFile))
         for fileName in [join(tempDir, fileObject.name()) for fileObject in outputFileList]:
             print("[+] Compiling '%s'" % fileName)
-            with open(os.devnull, "w") as devNull:
-                subprocess.check_call(["pdflatex", "-interaction=nonstopmode", "-output-directory", tempDir, fileName], stdout=devNull)
+            try:
+                if arguments.texOutput:
+                    subprocess.check_call(["pdflatex", "-interaction=nonstopmode", "-output-directory", tempDir, fileName])
+                else:
+                    with open(os.devnull, "w") as devNull:
+                        subprocess.check_call(["pdflatex", "-interaction=nonstopmode", "-output-directory", tempDir, fileName], stdout=devNull)
+            except subprocess.CalledProcessError as e:
+                print("[!] ERROR: pdflatex returned %d." % e.returncode, file=sys.stderr)
+                if not arguments.texOutput:
+                    print("[!] HINT: Run with --tex-output to see LaTeX output.", file=sys.stderr)
+                sys.exit(1)
             print("[+] ... done.")
             shutil.copy2(fileName + ".pdf", os.curdir)
+            if arguments.texOutput:
+                shutil.copy2(fileName, os.curdir)
 
 if __name__ == "__main__":
     main()
